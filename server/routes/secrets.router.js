3d100e4450d667e9c9b8097ca710b48325efa25e
Kyle Olson <kaollie74@gmail.com>
2019-07-31 16:12:52 -0500

    updated secrets router

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const pool = require('../modules/pool');
+// had to import this for step 1
 const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 const router = express.Router();
 
-
+// implemented rejectUnauthenticated in the get route
 router.get('/', rejectUnauthenticated, (req, res) => {
     console.log('req.user:', req.user);
-    pool.query(`SELECT * FROM "secret" WHERE secrecy_level < ${req.user.clearance_level};`)
+    // changed this query the current where the only the level of secrecy is returned that is
+    // less than the the current user clearance level
+    pool.query(`SELECT * FROM "secret" WHERE secrecy_level < $1;`, [req.user.clearance_level])
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
